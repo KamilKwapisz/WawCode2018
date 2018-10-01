4fd0cc453fc03c657ac70064a98ee84ded6c2796
Kamil Kwapisz <you@example.com>
2018-10-01 15:17:27 +0200

    auth views created

--- wawcode/chlanie/views.py
+++ wawcode/chlanie/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def profile(request):
+    context = dict(a="a")
+    return render(request, 'chlanie/profile.html', context)
--- wawcode/wawcode/settings.py
+++ wawcode/wawcode/settings.py
@@ -58,20 +58,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wawcode.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -102,8 +94,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
+
+DATE_INPUT_FORMATS = ('%d-%m-%Y')
+
+LOGIN_REDIRECT_URL = '/profile'
