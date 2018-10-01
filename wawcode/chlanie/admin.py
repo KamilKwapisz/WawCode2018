d1034232415acdbbf9df2e0c59b7f0ea3d10eb5b
Kamil Kwapisz <you@example.com>
2018-10-01 15:02:23 +0200

    few obligatory files and settings

--- wawcode/chlanie/admin.py
+++ wawcode/chlanie/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-# Register your models here.
+urlpatterns = [
+    path('', include('chlanie.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ wawcode/chlanie/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'chlanie'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
