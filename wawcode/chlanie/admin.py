edd7a7a6b4093845b8dca1542ec7ce2943e77c1c
Michał Maj <majkm4@gmail.com>
2018-10-05 10:06:27 +0200

    dodanie bazy danych

--- wawcode/chlanie/admin.py
+++ wawcode/chlanie/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .models import Uzytkownik, Lokal, Wlasciciel, WydarzenieUzytkownika, WydarzenieLokalu
 urlpatterns = [
     path('', include('chlanie.urls')),
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+admin.site.register(Uzytkownik)
+admin.site.register(Lokal)
+admin.site.register(Wlasciciel)
+admin.site.register(WydarzenieLokalu)
+admin.site.register(WydarzenieUzytkownika)
--- wawcode/chlanie/models.py
+++ wawcode/chlanie/models.py
@@ -2,9 +2,59 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
-
+class Uzytkownik(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.user.username)
+
+class WydarzenieUzytkownika(models.Model):
+	ID = models.AutoField(primary_key=True)
+	nick = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
+	tytul = models.CharField(max_length=40)
+	opis = models.CharField(max_length=100)
+	lokalizacjaNS = models.FloatField()
+	lokalizacjaWE = models.FloatField()
+	dataOd = models.DateTimeField()
+
+
+class Lokal(models.Model):
+	ID = models.AutoField(primary_key=True)
+	nazwa = models.CharField(max_length=40)
+	lokalizacjaNS = models.FloatField()
+	lokalizacjaWE = models.FloatField()
+	cenaPiwa = models.FloatField()
+	cenaWodki = models.FloatField()
+	jedzenie = models.BooleanField()
+	regionalne = models.BooleanField()
+	karaoke = models.BooleanField()
+	godzinyOtwarcia = models.TextField()
+	palarnia = models.BooleanField()
+	ogrodek = models.BooleanField()
+	ladowanieTelefonu = models.BooleanField()
+	parkiet = models.BooleanField()
+	mecze = models.BooleanField()
+
+class Komentarz(models.Model):
+	ID = models.AutoField(primary_key=True)
+	nick = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
+	ID_lokal = models.ForeignKey(Lokal, on_delete=models.CASCADE)
+	tekst = models.CharField(max_length=100)
+	ileGwiazdek = models.IntegerField()
+
+class WydarzenieLokalu(models.Model):
+	ID = models.AutoField(primary_key=True)
+	ID_lokal = models.ForeignKey(Lokal, on_delete=models.CASCADE)
+	tytul = models.CharField(max_length=40)
+	opis = models.CharField(max_length=100)
+	godzinaOd = models.DateTimeField()
+
+class Polubienie(models.Model):
+	ID = models.AutoField(primary_key=True)
+	ID_lokal = models.ForeignKey(Lokal, on_delete=models.CASCADE)
+	nick = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
+
+class Wlasciciel(models.Model):
+	wlasciciel = models.OneToOneField(User, on_delete=models.CASCADE)
+	ID_lokal = models.ForeignKey(Lokal, on_delete=models.CASCADE)
+
