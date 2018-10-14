667ed1d89ba87d43ec5b663527cee184773a920f
Kamil Kwapisz <kamiltm1998@gmail.com>
2018-10-14 13:32:07 +0200

    before pull

--- wawcode/wawcode/urls.py
+++ wawcode/wawcode/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('chlanie.urls')),
     path('', include('django.contrib.auth.urls')),
-    path('webpush/', include('webpush.urls')),
     path('admin/', admin.site.urls),
 ]
